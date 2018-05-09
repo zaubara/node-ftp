44332b5142f3b988cfbe0163bb6e10004231c868
Tamás András Horváth <htomy92@gmail.com>
2018-05-10 01:38:04 +0200

    Refactor test instances

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -4,8 +4,8 @@ describe('#Ftp Parser', () => {
   const { parseListEntry } = require('../lib/parser');
   const entires = require('./assets/entries');
 
-  it('should no exception while parsing', () => {
-    entires.forEach((entry) => {
+  entires.forEach((entry) => {
+    it(entry.what, () => {
       const result = parseListEntry(entry.source);
 
       assert.deepEqual(result, entry.expected);
